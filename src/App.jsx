8fb159cafa7001360263937bf024adc72965f7e2
Ronald Gutierrez <ronald654456@gmail.com>
2025-12-14 18:58:19 -0500

    Proyecto base Contradanza Paucartambo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-red-600 flex items-center justify-center">
+      <h1 className="text-6xl font-black text-white">
+        Contradanza Paucartambo
+      </h1>
+    </div>
+  )
+}
+
+export default App
